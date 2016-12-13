b8ee3941c68bc0a2b511b0bec8f6dd82e5fd79a5
gmp9 <gmpretto@sbcglobal.net>
2016-12-13 15:31:43 +0000

    added image + comments

---
+++ README.md
@@ -0,0 +1,12 @@
+ijing
+
+== README
+
+This is an app for consulting the yi jing
+The brush painting in the opening page represents Kan or the Water gua and is
+copied from Lama Govinda's book: The Inner Structure of the I Ching.
+
+This application requires:
+- Ruby 2.3.1
+- Rails 4.2.5
+- See the Gemfile
--- app/models/choice.rb
+++ app/models/choice.rb
@@ -14,9 +14,13 @@ class Choice
     arrayv ||= []
     n = 0
     
+    #convert the hash into an array 
     lines_array = lines.to_a
     
     while n < 6 do
+      #so that the method sample can be used to randomly pick an element
+      #6 elements need to be picked to represent the 6 lines of a gua
+      #giving us an array(array) which contains 6 smaller arrays
       select = lines_array.sample
       array.push(select)
       n += 1
